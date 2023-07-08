7d006030905bff581ebf70ddb1fa413d6651962f
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-08 09:51:28 +0530

    data file added

---
+++ src/components/data.js
@@ -0,0 +1,5 @@
+const movies = ['Suraj par mangal bhari', 'Tenet', 'The war with grandpa', 'The personal history of David Copperfield', 'Come Play']
+const slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"]
+const seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
+
+export { movies, slots, seats };
