8ca6546314a4be0a31ee8624ff020462575dc126
Ramesh_Chavan <ramesh7452@gmail.com>
2023-07-15 17:02:58 +0530

    initialized context

--- src/contextAPI/lastBooking.js
+++ src/contextAPI/lastBooking.js
@@ -1,4 +1,4 @@
 // Imported to create context
 import { createContext } from "react";
 // Created context to handle last movie booking and exported
-export const LastBookingContext = createContext();
+export const LastBookingContext = createContext("");
